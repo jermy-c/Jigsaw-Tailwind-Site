d504fa295b34b8747428082525d111ba227580f4
Jeremy <jermybz01@gmail.com>
2022-03-20 14:12:56 -0600

    add header styling

---
+++ source/_partials/header.blade.php
@@ -0,0 +1,4 @@
+<header class="text-center bg-green-500 lg:py-20 lg:px-24 md:py-12 md:px-16 py-8 px-4">
+    <h1 class="mt-0 mb-1 lg:text-6xl md:text-4xl text-3xl">{{ $page->title }}</h1>
+    <h2 class="mb-8 font-normal opacity-70 lg:text-xl md:text-lg text-base">{{ $page->description }}</h2>
+</header>
